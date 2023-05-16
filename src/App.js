72a7f75dcc4eb420150834d3aa5ed36e8f03f82f
Raghuvenkatasamy <raghukvr905@gmail.com>
2023-05-16 19:15:17 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          <h1>hi im Raghu</h1>
+          <h2>im a react developer</h2>
+          <h5>come and join with me for react devel</h5>
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React!!!!
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
